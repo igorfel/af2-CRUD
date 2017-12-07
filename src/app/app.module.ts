5b7b51c266db7456b88e9df7159e6aab5337ea8d
igorfel <appsigor@gmail.com>
2017-12-06 23:34:50 -0300

    Implementation of Create, Read, Update and Delete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,18 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase } from 'angularfire2/database';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyBHUJ5tr8h6K0XfDaZcpWHMvEKYSrXfGc0",
+  authDomain: "ionicprimeirospassos.firebaseapp.com",
+  databaseURL: "https://ionicprimeirospassos.firebaseio.com",
+  projectId: "ionicprimeirospassos",
+  storageBucket: "ionicprimeirospassos.appspot.com",
+  messagingSenderId: "161071887688"
+};
+
 @NgModule({
   declarations: [
     MyApp,
@@ -14,7 +26,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
+import { AngularFireDatabase, AngularFireList } from 'angularfire2/database';
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController, AlertController, ActionSheetController } from 'ionic-angular';
+import { Observable } from 'rxjs/Observable';
+import { database } from 'firebase/app';
+import { Title } from '@angular/platform-browser/src/browser/title';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +11,108 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  songsList: AngularFireList<any>;
+  songs: Observable<any[]>
 
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController, public actionSheetCtrl: ActionSheetController, public afDatabase: AngularFireDatabase) {
+    this.songsList = afDatabase.list('/songs');
+    this.songs = this.songsList.valueChanges();
   }
 
+  addSong(){
+    let prompt = this.alertCtrl.create({
+      title: 'Song Name',
+      message: "Enter the name of this new song",
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Title'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log("Operation cancelled");
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            const newSongRef = this.songsList.push({})
+
+            newSongRef.set({
+              id: newSongRef.key,
+              title: data.title
+            })
+          }
+        }
+      ]
+    });
+
+    prompt.present();
+  }
+
+  showOptions(songId, songTitle){
+    let actionSheet = this.actionSheetCtrl.create({
+      title: 'What do you want to do?',
+      buttons: [
+        {
+          text: 'Delete Song',
+          role: 'destructive',
+          handler: () => {
+            this.removeSong(songId);
+          }
+        },{
+          text: 'Update Title',
+          handler: () => {
+            this.updateSong(songId, songTitle);
+          }
+        },{
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('Operation cancelled');
+          }
+        }
+      ]
+    });
+
+    actionSheet.present();
+  }
+
+  removeSong(songId: string){
+    this.songsList.remove();
+  }
+
+  updateSong(songId: string, songTitle: string){
+    let prompt = this.alertCtrl.create({
+      title: 'Song Name',
+      message: 'Update the name of this song',
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Title',
+          value: songTitle
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Operation Cancelled');
+          }
+        },{
+          text: 'Save',
+          handler: data => {
+            this.songsList.update(songId, {
+              title: data.title
+            });
+          }
+        }
+      ]
+    });
+
+    prompt.present();
+  }
 }
